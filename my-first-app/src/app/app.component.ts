d8e4c8fc3d8732e918924fd37faf259117a18285
adwaitjoshi009 <adwaitjoshi009@gmail.com>
2022-12-03 15:27:13 +0530

    learnt about app.module.ts

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // can include inline template by templaete: '<app-component></app-component>
   styleUrls: ['./app.component.css']
+  // styleUrls is an [], so multiple links possible
+  // also, inline styling is also allowed
 })
 export class AppComponent {
-  title = 'Angular App'
-  name = 'Adwait'
+  // title = 'Angular App'
+  // name = 'Adwait'
 }
