136801ddfc6b2642a0361115df37abb974a5e191
adwaitjoshi009 <adwaitjoshi009@gmail.com>
2022-12-07 13:21:39 +0530

    removed the steps file

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -7,24 +7,27 @@ import { Component, OnInit } from '@angular/core';
   //             <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent implements OnInit {ls
+export class ServersComponent implements OnInit {
   allowNewServer = true
   serverStatus = 'No server is created.'
   serverName = 'Vesper'
   serverPresent = false
+  servers = ['Testserver1', 'Testserver2']
+  someArray = [1, 2, 3, 4, 5]
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = !this.allowNewServer
     }, 2000)
   }
-
+  
   ngOnInit(): void {
   }
-
+  
   onCreateServer() {
     this.serverStatus = 'New Server is added. Name is: ' + this.serverName
     this.serverPresent = true
+    this.servers.push(this.serverName)
   }
 
   onUpdateServerName(event: Event) {
